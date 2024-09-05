20288b5fbab4e4f9f5f48ce11ea29596f6f9ed19
ryan <vky1887@autuni.ac.nz>
2024-09-05 20:55:40 +1200

    update

--- golf-project/golf-project/src/App.jsx
+++ golf-project/golf-project/src/App.jsx
@@ -8,6 +8,7 @@ function App() {
 
   return (
     <>
+    <Navbar /> {/* Navbar component*/}
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
