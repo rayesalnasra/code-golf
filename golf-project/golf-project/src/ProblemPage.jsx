043f2c1bca246d96707c27371310311a0acf52e1
rayesalnasra <130417264+rayesalnasra@users.noreply.github.com>
2024-09-10 10:41:12 +1200

    Added CodeMirror component for code editing

--- golf-project/golf-project/src/ProblemPage.jsx
+++ golf-project/golf-project/src/ProblemPage.jsx
@@ -2,11 +2,30 @@ import React, { useState } from "react";
 import CodeMirror from "@uiw/react-codemirror";
 import { python } from "@codemirror/lang-python";
 import axios from "axios";
-function ProblemPage() {
-  return (
-    <>
-    </>
-  )
-}
 
-export default ProblemPage
+export default function ProblemPage() {
+    // State to hold the code from the editor
+    const [code, setCode] = useState("");
+  
+    // Update the code state when editor content changes
+    const handleChange = React.useCallback((value) => {
+      setCode(value);
+    }, []);
+  
+    return (
+      <div>
+        <h1>Python Code Tester</h1>
+        <div>Create a function that adds two numbers in Python</div>
+        
+        {/* CodeMirror editor for writing Python code */}
+        <CodeMirror
+          value={code} // Initial content of the editor
+          height="200px" // Height of the editor
+          theme="dark" // Theme for the editor
+          extensions={[python({ jsx: true })]} // Python syntax highlighting
+          onChange={handleChange} // Function to call when the editor content changes
+        />
+      </div>
+    );
+  }
+  
