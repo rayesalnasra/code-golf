041e6cfb61bb14b81e2acb4b09306b3058ba6a2f
rayesalnasra <130417264+rayesalnasra@users.noreply.github.com>
2024-09-10 10:11:40 +1200

    Integrate CodeMirror Python language support

--- golf-project/golf-project/src/ProblemPage.jsx
+++ golf-project/golf-project/src/ProblemPage.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
 import CodeMirror from "@uiw/react-codemirror";
 
-
 function ProblemPage() {
   return (
     <>
