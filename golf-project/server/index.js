b8f3be16e45421a135040633b235b77afeaf6ead
rayesalnasra <130417264+rayesalnasra@users.noreply.github.com>
2024-09-10 11:11:48 +1200

    Added CORS support

--- golf-project/server/index.js
+++ golf-project/server/index.js
@@ -1,11 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 const port = 80;
 
+app.use(cors());
 app.use(express.json());
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 });
-
