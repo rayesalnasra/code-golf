dc7720c2d6932e3460f53be5ea07bd922de159e3
rayesalnasra <130417264+rayesalnasra@users.noreply.github.com>
2024-09-17 16:56:38 +1200

    .

--- golf-project/server/user_code.py
+++ golf-project/server/user_code.py
@@ -1,2 +1,6 @@
 def is_palindrome(s):
-    # Your code here
+    # Remove non-alphanumeric characters and convert to lowercase
+    cleaned_str = ''.join(char.lower() for char in s if char.isalnum())
+    
+    # Check if the cleaned string is equal to its reverse
+    return cleaned_str == cleaned_str[::-1]
